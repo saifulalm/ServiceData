a562963911317d4b2e24e4e5ba60c9a91bd4c91e
saifulalm <ipoela@gmail.com>
2022-07-06 00:43:11 +0700

    Update

--- app/Services/ServiceData/GetRequest.php
+++ app/Services/ServiceData/GetRequest.php
@@ -29,7 +29,7 @@ class GetRequest
         $sign=md5($credential['apikey'].$credential['secret'].$timestamp);
         $header = ['api_key' => $credential['apikey'], 'x-signature' => $sign];
         event(new RequestEvent(json_encode($data)));
-        $response = Http::withHeaders($header)->get('http://68.183.188.18:3010/api/v0/balance', $data);
+        $response = Http::withHeaders($header)->get('http://68.183.188.18:3010/api/v0/balance', $data)->json();
         event(new ResponseEvent($response));
 
         return $response;
