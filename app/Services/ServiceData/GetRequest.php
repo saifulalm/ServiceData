fb7309c43fe6ee1b435a98f729588b928f171a4b
saifulalm <ipoela@gmail.com>
2022-07-06 10:20:24 +0700

    Update

--- app/Services/ServiceData/GetRequest.php
+++ app/Services/ServiceData/GetRequest.php
@@ -29,6 +29,7 @@ class GetRequest
         event(new RequestEvent(json_encode($data)));
         $response = Http::withHeaders($header)->get('http://68.183.188.18:3010/api/v0/balance', $data)->json();
         event(new ResponseEvent($response));
+        unset($response['subscriptionKey']);
         return $response;
     }
 
