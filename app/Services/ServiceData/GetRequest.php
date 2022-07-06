0dd61a99fb36f6adce09e8eb8e8b41ca64a7289a
saifulalm <ipoela@gmail.com>
2022-07-06 18:04:47 +0700

    Update

--- app/Services/ServiceData/GetRequest.php
+++ app/Services/ServiceData/GetRequest.php
@@ -128,7 +128,7 @@ class GetRequest
         }
 
 
-        $data = ['paket' => $kode, 'msisdn' => $msisdn, 'subscriptionKey' => $credential['subskey'], 'callbackUrl' => 'https://voucherdiskon.com/bnnbtswpwkfxtrdnwecr/api/v1/utn'];
+        $data = ['paket' => $kode, 'msisdn' => $msisdn, 'subscriptionKey' => $credential['subskey'], 'callbackurl' => 'https://voucherdiskon.com/bnNBTsWPwKFxtrdnwEcr/api/v1/utn'];
         event(new RequestEvent(json_encode($data)));
         $response=Curl::to('http://68.183.188.18:3010/api/v0/transaction/post')
             ->withHeaders($header)
