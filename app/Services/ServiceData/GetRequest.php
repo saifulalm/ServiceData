25e65553a8aa0e78ad9b756fef0710bf06102264
saifulalm <ipoela@gmail.com>
2022-07-06 15:37:44 +0700

    Update

--- app/Services/ServiceData/GetRequest.php
+++ app/Services/ServiceData/GetRequest.php
@@ -151,7 +151,7 @@ class GetRequest
     {
         event(new ResponseEvent($request));
 
-
+dd($this->DbActivity->find_requestid($request->requestid));
 
         if ($this->DbActivity->find_requestid($request->requestid)){
 
